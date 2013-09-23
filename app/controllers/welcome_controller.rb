24e27868288f3b0d089709f7b023d622c1f5b288
Andrew Emelianenko <emelyanenko.web@gmail.com>
2013-09-24 01:58:27 +0300

    add base google SSO [2]

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    if user_signed_in?
+      Google::User.all(current_user.domain)
+    end
+  end
+end
