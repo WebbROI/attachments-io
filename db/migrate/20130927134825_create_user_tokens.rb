77e555001261244047cb20e874d9f0d123d7ff2e
Andrew Emelianenko <emelyanenko.web@gmail.com>
2013-10-01 01:03:29 +0300

    modify migration

--- db/migrate/20130927134825_create_user_tokens.rb
+++ db/migrate/20130927134825_create_user_tokens.rb
@@ -2,7 +2,7 @@ class CreateUserTokens < ActiveRecord::Migration
   def change
     create_table :user_tokens do |t|
       t.string  :user_id
-      t.string  :token
+      t.string  :access_token
       t.string  :refresh_token
       t.integer :expires_at
     end
--- db/migrate/20130930171456_rename_token_to_access_token.rb
+++
@@ -1,5 +0,0 @@
-class RenameTokenToAccessToken < ActiveRecord::Migration
-  def change
-    rename_column :user_tokens, :token, :access_token
-  end
-end
