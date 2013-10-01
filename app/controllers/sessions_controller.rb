aa06d16beaca2627fa9f3b7c3347a1c683ba3e86
Andrew Emelianenko <emelyanenko.web@gmail.com>
2013-10-01 22:00:10 +0300

    first login -> start synchronization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,7 @@ class SessionsController < ApplicationController
       # TODO: add load information
       # @user.load_info
 
-      redirect_to root_path, flash: { success: 'Thank you for using our service! Synchronization was started!' }
+      redirect_to sync_start_path, flash: { success: 'Thank you for using our service! Synchronization was started!' }
     else
       redirect_to root_path, flash: { success: 'You successful login!' }
     end
