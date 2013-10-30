8ac0569f93531c71ed6b421960234b4b4a3f666c
Andrew Emelianenko <emelianenko.web@gmail.com>
2013-10-30 13:23:47 +0200

    new design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets//sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-2.0.3.min
+//= require vendor/jquery
 //= require jquery_ujs
-//= require semantic.min
+//= require vendor/semantic
+//= require vendor/retina
 //= require_tree .
 
 //
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  layout 'no-wrapper'
+
   def home
     if user_signed_in?
       redirect_to profile_path
