2ff7b47cf48f1a9f417e567df07bc2e0d40176c4
Andrew Emelianenko <emelianenko.web@gmail.com>
2013-11-25 11:31:54 +0200

    fix assets [1]

---
+++ vendor/assets/javascripts/libs.js
@@ -0,0 +1,10 @@
+//= require jquery_ujs
+//= require retina_tag
+//= require jquery.livequery.min.js
+//= require twitter/bootstrap/alert
+//= require twitter/bootstrap/dropdown
+
+// FIXME: Max, please fix this ;)
+// fix dropdown
+$('.dropdown').dropdown();
+
