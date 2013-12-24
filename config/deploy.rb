361cfe976139b532fd4431d8e44dfdca142efb6b
Andrew Emelianenko <emelianenko.web@gmail.com>
2013-12-24 13:48:23 +0200

    restart unicorn and app after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,8 +20,8 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      run 'service unicorn restart'
+      run 'service attachments-io restart'
     end
   end
 
