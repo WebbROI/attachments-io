46c9fcde0e500bd2715606224d6e8563ca830531
Andrew Emelianenko <emelianenko.web@gmail.com>
2014-02-20 00:52:53 +0200

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,21 @@
-listen '127.0.0.1:8080'
-
 worker_processes 3
 working_directory '/home/rails/current'
+timeout 30
 
 user 'rails'
 
 pid '/home/unicorn/pids/unicorn.pid'
+listen '/home/unicorn/sockets/unicorn.sock'
 
 stderr_path '/home/unicorn/log/unicorn.log'
 stdout_path '/home/unicorn/log/unicorn.log'
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
+end
