01bd0f9b42066d1a371478d99db63ad78f0f331a
Andrew Emelianenko <emelianenko.web@gmail.com>
2014-02-19 18:19:08 +0200

    capistrano staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,15 @@
 set :application, '@ttachments.io'
 set :repo_url, 'git@github.com:WebbROI/attachments-io.git'
-set :branch, 'master'
-
-set :deploy_to, '/home/rails'
+set :pty, true
+set :keep_releases, 1
 set :scm, :git
 
-# set :format, :pretty
-# set :log_level, :info
-set :pty, true
+task :production do
+  set :branch, 'master'
+  set :deploy_to, '/home/rails'
+end
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { rvm_bin_path: '/usr/local/rvm/bin' }
-set :keep_releases, 1
-
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      #execute "cd #{current_path} && sudo service unicorn restart"
-      #execute "cd #{current_path} && sudo service attachments-io restart"
-    end
-  end
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
+task :test do
+  set :branch, 'test'
+  set :deploy_to, '/home/rails-test'
 end
