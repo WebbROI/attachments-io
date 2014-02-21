87f8e3c123949ec18893b7784ea569a567d2f4b3
Andrew Emelianenko <emelianenko.web@gmail.com>
2014-02-21 14:16:58 +0200

    update unicorn & capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,15 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'attachments.io'
+set :application, 'app.attachments.io'
 set :repo_url, 'git@github.com:WebbROI/attachments-io.git'
+set :rvm_type, :user
+set :rvm_ruby_version, '2.1.0'
 
-role :web, %w{root@162.243.44.145}
-role :app, %w{root@162.243.44.145}
-role :db, %w{root@162.243.44.145}
-server '162.243.44.145', user: 'root', roles: %w{web app db}
+# role :web, %w{root@162.243.44.145}
+# role :app, %w{root@162.243.44.145}
+# role :db, %w{root@162.243.44.145}
+server '162.243.44.145', user: 'rails', roles: %w{web app db}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -39,25 +41,83 @@ server '162.243.44.145', user: 'root', roles: %w{web app db}
 # Default value for keep_releases is 5
 set :keep_releases, 1
 
+namespace :foreman do
+  desc 'Start application'
+  task :start do
+    on roles(:all) do
+      sudo "start #{application}"
+    end
+  end
+
+  desc 'Stop application'
+  task :stop do
+    on roles(:all) do
+      sudo "stop #{application}"
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:all) do
+      sudo "restart #{application}"
+    end
+  end
+
+  desc 'Application status'
+  task :status do
+    on roles(:all) do
+      sudo "status #{application}"
+    end
+  end
+end
+
 namespace :deploy do
+
+  desc 'Setup application'
+  task :setup do
+    on roles(:all) do
+      execute "mkdir /var/www/apps/#{application}/run/"
+      execute "mkdir /var/www/apps/#{application}/log/"
+      execute "mkdir /var/www/apps/#{application}/socket/"
+    end
+  end
+
+  desc 'Initialize foreman'
+  task :foreman_init do
+    on roles(:all) do
+      foreman_temp = '/var/www/tmp/foreman'
+      execute "mkdir -p #{foreman_temp}"
+      execute "ln -s #{release_path} #{current_path}"
+
+      within current_path do
+        execute "cd #{current_path}"
+        execute :bundle, "exec foreman export upstart #{foreman_temp} -a #{application} -u rails -l /var/www/apps/#{application}/log -d #{current_path}"
+      end
+
+      sudo "mv #{foreman_temp}/* /etc/init/"
+      sudo "rm -r #{foreman_temp}"
+    end
+  end
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      sudo "restart #{application}"
     end
   end
 
   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  after :finishing, 'deploy:cleanup'
+  after :finishing, 'deploy:restart'
 
+  after :setup, 'deploy:foreman_init'
+
+  # after :foreman_init, 'foreman:start'
+
+  before :foreman_init, 'rvm:hook'
+
+  before :setup, 'deploy:starting'
+  before :setup, 'deploy:updating'
+  before :setup, 'bundler:install'
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+application = 'app.attachments.io'
+
 set :stage, :production
 set :branch, 'master'
-set :deploy_to, '/home/rails'
+set :deploy_to, '/var/www/apps/app.attachments.io'
