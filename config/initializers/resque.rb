a43a34f0530bc583b2dd6b9ff9a48ac95405d0fa
Andrew Emelianenko <emelianenko.web@gmail.com>
2014-02-21 16:18:20 +0200

    update password for redis [2]

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,11 +1,9 @@
 Dir["#{Rails.root}/app/workers/*.rb"].each { |file| require file }
 
-if Rails.env.development?
-  Resque::Server.use(Rack::Auth::Basic) do |user, password|
-    password == 'Chargers36@'
-  end
-else
-  Resque::Server.use(Rack::Auth::Basic) do |user, password|
-    password == 'Chargers36!@'
-  end
+Resque::Server.use(Rack::Auth::Basic) do |user, password|
+  password == 'Chargers36@'
+end
+
+unless Rails.env.development?
+  Resque.redis = Redis.new(password: 'Chargers36!@')
 end
