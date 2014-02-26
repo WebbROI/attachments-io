0f92f9ba1afa190f656a460b05d404a010632038
Andrew Emelianenko <emelianenko.web@gmail.com>
2014-02-26 02:34:35 +0200

    fix task user:fix

--- app/models/user_synchronization.rb
+++ app/models/user_synchronization.rb
@@ -24,7 +24,7 @@ class UserSynchronization < ActiveRecord::Base
     problematic = []
 
     where(status: Synchronization::INPROCESS).each do |sync|
-      if sync.started_at.nil? || sync.started_at.to_i - Time.now.to_i > 1.hours
+      if sync.started_at.nil? || Time.now.to_i - sync.started_at.to_i > 1.hours.to_i
         problematic << sync
       end
     end
