c5ce57d92f325c922faf3579be567aee364a2c46
Andrew Emelianenko <emelianenko.web@gmail.com>
2014-06-17 12:33:02 +0300

    update .gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,26 +6,26 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user_session, :current_user, :user_signed_in?
 
-  private
-    def current_user_session
-      return @current_user_session if defined? @current_user_session
-      @current_user_session = UserSession.find
-    end
+  private 
+  def current_user_session
+    return @current_user_session if defined? @current_user_session
+    @current_user_session = UserSession.find
+  end
 
-    def current_user
-      return @current_user if defined? @current_user
-      @current_user = current_user_session && current_user_session.user
-    end
+  def current_user
+    return @current_user if defined? @current_user
+    @current_user = current_user_session && current_user_session.user
+  end
 
-    def user_signed_in?
-      !!current_user
-    end
+  def user_signed_in?
+    !!current_user
+  end
 
-    def authenticate_user!
-      redirect_to root_path, flash: { error: 'You need sign in to view this page' } unless user_signed_in?
-    end
+  def authenticate_user!
+    redirect_to root_path, flash: { error: 'You need sign in to view this page' } unless user_signed_in?
+  end
 
-    def not_authenticated_user!
-      redirect_to profile_path if user_signed_in?
-    end
+  def not_authenticated_user!
+    redirect_to profile_path if user_signed_in?
+  end
 end
